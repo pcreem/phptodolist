fe5b4a0e84d867a5e265174bc91f0a2ed6214461
pcreem <pcreem@gmail.com>
2021-01-25 16:00:40 +0800

    create mysql table

--- test.php
+++ test.php
@@ -9,36 +9,63 @@
     <?php 
         $servername = "localhost";
         $username = "localuser";
-        $password = "Password123#@!";
+				$password = "Password123#@!";
+				$dbname = 'todolist';
         
         // Create connection
-        $conn = new mysqli($servername, $username, $password);
-        
-        // Check connection
-        if ($conn->connect_error) {
-          die("Connection failed: " . $conn->connect_error);
-        }
-        echo "Connected successfully";
+				$conn = new mysqli($servername, $username, $password, $dbname);
+				// Check connection
+				if ($conn->connect_error) {
+					die("Connection failed: " . $conn->connect_error);
+				}
 
-        if (isset($_POST['submit'])) {
-            if (empty($_POST['task'])) {
-                $errors = "You must fill in the task";
-            }else{
-                $task = $_POST['task'];
-                $sql = "INSERT INTO tasks (task) VALUES ('$task')";
-                mysqli_query($conn, $sql);
-                header('location: test.php');
-            }
-        }	
+				// sql to create table
+				$sql = "CREATE TABLE Task (
+				id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+				task VARCHAR(30) NOT NULL,
+				reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+				)";
+
+				if ($conn->query($sql) === TRUE) {
+					echo "Table Task created successfully";
+				} else {
+					echo "Error creating table: " . $conn->error;
+				}
+
+				$conn->close();
     ?>
 
 	<div class="heading">
 		<h2 style="font-style: 'Hervetica';">ToDo List</h2>
 	</div>
-	<form method="post" action="index.php" class="input_form">
+	<form method="post" action="test.php" class="input_form">
 		<input type="text" name="task" class="task_input">
 		<button type="submit" name="submit" id="add_btn" class="add_btn">Add Task</button>
     </form>
-    <p>test seeeer</p>
+    <table>
+	<thead>
+		<tr>
+			<th>N</th>
+			<th>Tasks</th>
+			<th style="width: 60px;">Action</th>
+		</tr>
+	</thead>
+
+	<tbody>
+		<?php 
+		// select all tasks if page is visited or refreshed
+		$tasks = mysqli_query($conn, "SELECT * FROM tasks");
+
+		$i = 1; while ($row = mysqli_fetch_array($tasks)) { ?>
+			<tr>
+				<td> <?php echo $i; ?> </td>
+				<td class="task"> <?php echo $row['task']; ?> </td>
+				<td class="delete"> 
+					<a href="index.php?del_task=<?php echo $row['id'] ?>">x</a> 
+				</td>
+			</tr>
+		<?php $i++; } ?>	
+	</tbody>
+</table>
 </body>
 </html>
